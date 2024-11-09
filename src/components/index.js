dde668dfe9ee17bbc8dceb87adf375166e38d15e
siddhantmishra0 <mishrasiddhant2605@gmail.com>
2024-11-09 13:17:04 +0530

    ..

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,17 @@
-import Select from "./Select";
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
-import Container from "./container/Container";
-import Logo from "./Logo";
-import LogoutBtn from "./Header/LogoutBtn";
-import RTE from "./RTE";
-import Signup from "./Signup";
-import Login from "./Login";
-import Button from "./Button";
-import PostForm from "./post-form/PostForm";
-import PostCard from "./PostCard";
-import AuthLayout from "./AuthLayout";
-import {Input} from "./Input";
+import Select from "./Select.jsx";
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Container from "./container/Container.jsx";
+import Logo from "./Logo.jsx";
+import LogoutBtn from "./Header/LogoutBtn.jsx";
+import RTE from "./RTE.jsx";
+import Signup from "./Signup.jsx";
+import Login from "./Login.jsx";
+import Button from "./Button.jsx";
+import PostForm from "./post-form/PostForm.jsx";
+import PostCard from "./PostCard.jsx";
+import AuthLayout from "./AuthLayout.jsx";
+import {Input} from "./Input.jsx";
 export {
     Header,
     Footer,
